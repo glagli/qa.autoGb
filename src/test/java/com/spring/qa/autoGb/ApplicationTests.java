4d5dc97db8f9e813d6496a91c031b9174bbc7cc0
Vladislav <Glagli@yandex.ru>
2022-09-23 08:46:42 +0300

    init commit

---
+++ src/test/java/com/spring/qa/autoGb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.qa.autoGb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
